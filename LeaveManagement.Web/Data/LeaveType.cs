41906d6e45a4c2435d8f797d13ff0c150d5000bc
Nat Ervin <nate_thomas_95@hotmail.com>
2024-05-02 14:58:30 +0930

    Visual layout changes and adding CRUD functionality

--- LeaveManagement.Web/Data/LeaveType.cs
+++ LeaveManagement.Web/Data/LeaveType.cs
@@ -1,8 +1,13 @@
-﻿namespace LeaveManagement.Web.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.Data
 {
     public class LeaveType : BaseEntity
     {
+        [Display(Name = "Leave Type Name")]
         public string Name { get; set; }
+
+        [Display(Name = "Default Number of Days")]
         public int DefaultDays { get; set; }
     }
 }
